263b01874957748828c3935bb1309da298920ce6
LaurenceGab <rihitosenpailitch@gmail.com>
2025-09-19 20:12:31 +0800

    first commit

---
+++ FileOperation.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <cstring>
+#include <sys/stat.h>
+#include <conio.h>   
+
+
+void setFilePermissions(const char* filename, mode_t mode) {
+    if (chmod(filename, mode) != 0) {
+        perror("Error setting file permissions");
+    } else {
+        std::cout << filename << " permissions updated successfully." << std::endl;
+    }
+}
+
+int main() {
+    char filename[50];
+    int choice;
+
+    std::cout << "MAIN MENU OF PERMISSION\n";
+    std::cout << "---------------------------------\n";
+    std::cout << "1. Only Read\n";
+    std::cout << "2. Only Write\n";
+    std::cout << "3. Exit\n";
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
+
+    switch (choice) {
+        case 1:
+            std::cout << "Enter the file name: ";
+            std::cin >> filename;
+            setFilePermissions(filename, S_IRUSR); 
+            break;
+
+        case 2:
+            std::cout << "Enter the file name: ";
+            std::cin >> filename;
+            setFilePermissions(filename, S_IWUSR); 
+            break;
+
+        case 3:
+            exit(0);
+            break;
+
+        default:
+            std::cout << "Invalid choice." << std::endl;
+    }
+
+    getch(); 
+    return 0;
+}
